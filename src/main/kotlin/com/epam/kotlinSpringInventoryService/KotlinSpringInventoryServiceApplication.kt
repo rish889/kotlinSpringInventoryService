ae434001593e69d45260393906152afb60afbddd
Rishabh Garcha <rish889@gmail.com>
2024-04-24 18:42:48 +0530

    Initial Commit

---
+++ src/main/kotlin/com/epam/kotlinSpringInventoryService/KotlinSpringInventoryServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.epam.kotlinSpringInventoryService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringInventoryServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringInventoryServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/epam/kotlinSpringInventoryService/KotlinSpringInventoryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.epam.kotlinSpringInventoryService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringInventoryServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
