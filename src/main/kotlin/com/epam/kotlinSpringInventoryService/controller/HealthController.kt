5e94f44a47f642008a4d1c1988abed1db6edcd66
Rishabh Garcha <rish889@gmail.com>
2024-04-24 18:57:16 +0530

    Initial Commit

---
+++ src/main/kotlin/com/epam/kotlinSpringInventoryService/controller/HealthController.kt
@@ -0,0 +1,13 @@
+package com.epam.kotlinSpringInventoryService.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+
+    @GetMapping("/health-check")
+    fun healthCheck(): String {
+        return "Kotlin Spring Inventory Service Healthy"
+    }
+}
